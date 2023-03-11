c7bf408af5dd7d66ea589b385652a9f2130aca8b
silashorta <92862835+silashorta@users.noreply.github.com>
2023-03-11 15:14:09 -0300

    update menu

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const botaoAbrir = document.querySelector('#botao-menu');
-const menu = document.querySelector('.menu-lista');
+const menuBtn = document.getElementById('botao-menu');
+const menu = document.querySelector('.menu__lista-resp');
+const menuIcon = menuBtn.querySelector('img');
 
-botaoAbrir.addEventListener('click', () => {
-  menu.style.display = 'menu-lista';
+menuBtn.addEventListener('click', function() {
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+  } else {
+    menu.style.display = 'block';
+    menuIcon.src = 'icon/icons8-fechar-janela-50.png';
+  }
 });
+
