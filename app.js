287a0a7684fc707880144c7365b1d34f9c8ca505
silashorta <92862835+silashorta@users.noreply.github.com>
2023-03-11 15:37:24 -0300

    update css

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const menuIcon = menuBtn.querySelector('img');
 menuBtn.addEventListener('click', function() {
   if (menu.style.display === 'block') {
     menu.style.display = 'none';
+    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
     menuIcon.classList.remove('rotated');
   } else {
     menu.style.display = 'block';
+    menuIcon.src = 'icon/icons8-excluir-24.png';
     menuIcon.classList.add('rotated');
   }
 });
