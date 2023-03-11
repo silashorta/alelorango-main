46c94bc5b736d98aeb234d40daa39e292c6e1d74
silashorta <92862835+silashorta@users.noreply.github.com>
2023-03-11 16:59:41 -0300

    update v4

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 const menuBtn = document.getElementById('botao-menu');
 const menu = document.querySelector('.menu__lista-resp');
-const menuIcon = menuBtn.querySelector('img');
+let menuIcon = menuBtn.querySelector('img');
 
-menuBtn.addEventListener('click', function() {
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
-    menuIcon.classList.remove('rotated');
-  } else {
-    menu.style.display = 'block';
-    menuIcon.src = 'icon/icons8-excluir-24.png';
-    menuIcon.classList.add('rotated');
-  }
+menuBtn.addEventListener('click', function () {
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+        menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+        menuIcon.classList.remove('rotated');
+    } else {
+        menu.style.display = 'block';
+        menuIcon.src = 'icon/icons8-excluir-24.png';
+        menuIcon.classList.add('rotated');
+    }
 });
 
+
+const comoParticipar = document.querySelector('#botao-como-participar');
+const botaoPremios = document.querySelector('#botao-premios')
+comoParticipar.addEventListener('click', function () {
+    console.log('clicado')
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+        menuIcon.src = 'icon/icons8-excluir-24.png';
+    } else {
+        menu.style.display = 'block';
+        menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+    }
+    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+});
+botaoPremios.addEventListener('click', function () {
+    console.log('clicado')
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+
+    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+});
+const botaoRegulamento = document.querySelector('#botao-regulamento')
+botaoRegulamento.addEventListener('click', function () {
+    console.log('clicado')
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+
+    menuIcon.src = 'icon/icons8-menu-arredondado-30.png';
+});
 
 
 
